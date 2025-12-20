5fa54bf16fe1b7106da40a824cabdd5dc71379d8
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-20 17:20:53 -0300

    Add project files.

---
+++ Aula01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando no .NET");
+        }
+    }
+}
---
+++ Aula02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Antônio";
+
+            int number = 20;
+            double numberDouble = 10.50;
+            char character = 'A';
+            bool boolean = true;
+
+            //----------------------
+
+            Console.WriteLine(numberDouble);
+        }
+    }
+}
