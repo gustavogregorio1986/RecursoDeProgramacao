c760aa5596777186bc5f843ac4f760b5dd144553
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-21 16:40:03 -0300

    Desnevolvendo paar o  crescimento profissional, sempre evoluindo da melhor forma!

---
+++ Aula06/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================Verificador de Numeros pares ou impares==============");
+
+            Console.WriteLine("Diogite um numero: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = number % 2;
+
+
+            if (result == 0)
+            {
+                Console.WriteLine($"O numero {number} é par");
+            }
+            else
+            {
+                Console.WriteLine($"O numero {number} é impar");
+            }
+        }
+    }
+}
---
+++ Aula07/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Aula07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==============Operadores de Aytribuição===============");
+
+            //Inicializar a variavel
+            int x = 30;
+            Console.WriteLine("Valor inicial da Variavel de x? " + x);
+
+            //Operadores de Atribuição
+            x = 20;
+
+            Console.WriteLine("Atribuição Simples (=)");
+            Console.WriteLine("Valor de x: " + x);
+
+            //Atribuição Completa (incremento)
+            x++; // Equivale a x = x + 1
+
+            Console.WriteLine("\n Atribuição Composta Incremento (++)");
+            Console.WriteLine("Novo valor sw x:  (++) " + x);
+
+            //Atribuição Completa (+=)
+            x += 10; // Equivale a x = x + 1
+
+            Console.WriteLine("\n Atribuição Composta Incremento (++)");
+            Console.WriteLine("Novo valor sw x:  (++) " + x);
+
+            //Atribuição Completa (-=)
+            x -= 10; // Equivale a x = x - 1
+
+            Console.WriteLine("\n Atribuição Composta Incremento (++)");
+            Console.WriteLine("Novo valor sw x:  (++) " + x);
+        }
+    }
+}
