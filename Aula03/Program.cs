f7585ec7db1a45aaab47cff279a6fc23fb947dc6
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-20 21:51:16 -0300

    Desnevolver sempre é a arte de programara paar evoluir profissionalmente sempre!

---
+++ Aula03/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Aula03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string name = "Antônio";
+
+            name = "Antonio Rocha";
+
+
+            Console.WriteLine("O valor da antiga da variavel é: " + name);
+
+            name = "Antônio Junior";
+
+            Console.WriteLine("O valor novo da variavel é: " + name);
+
+            const int number = 1000;
+
+            Console.WriteLine("O valor da constante é: " + number);
+
+             string variable = "Antònio";
+
+            variable = "Rocha";
+
+            Console.WriteLine("O valor da constante é: " + variable);
+            */
+
+            //var
+            // dynamic
+            //obj
+
+            dynamic variable = 10;
+
+            variable = "Antônio";
+
+            Console.WriteLine("O valor da variavle é: " + variable);
+
+        }
+    }
+}
---
+++ Aula04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------CADASTRO DE USUÁRIO---------------");
+
+            //Solicitar o nome do usuario
+            Console.WriteLine("Digite o Seu Nome: ");
+            string nome = Console.ReadLine();
+
+            //Solicitar a idade do usuario
+            Console.WriteLine("Digite Sua idade: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------------------------");
+
+            Console.WriteLine("Nome do Usuário: " + nome);
+            Console.WriteLine("Ida do usuario: " + age);
+
+        }
+    }
+}
