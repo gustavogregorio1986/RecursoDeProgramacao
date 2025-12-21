ad83dc6276e1fb53a58fbd444b7db01c8bc4769c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-21 17:07:08 -0300

    Desenvolver a base da linguagem C# paar sempre evoluir profissionalmente , sempre o conceitro deve ser visitado

---
+++ Aula08/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Aula08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============Operadores não relacionais================ \n");
+
+            Console.WriteLine("Digite o primeiro numero: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            bool result;
+
+            // Operador de igualdade (==)
+            if (x == y)
+            {
+                Console.WriteLine($"Os numeros {x} e {y} são iguais.");
+            }
+            else
+            {
+                Console.WriteLine($"Os numeros {x} e {y} não são diferentes.");
+            }
+
+            // Operador de igualdade (==)
+            if (x != y)
+            {
+                Console.WriteLine($"Os numeros {x} e {y} são difeentes.");
+            }
+            else
+            {
+                Console.WriteLine($"Os numeros {x} e {y} não são iguais.");
+            }
+
+            if (x >= y)
+            {
+                Console.WriteLine($"{x} é menor ou igual que {y}.");
+            }
+            else
+            {
+                Console.WriteLine($"{x} é maior ou igual que {y}.");
+            }
+        }
+    }
+}
