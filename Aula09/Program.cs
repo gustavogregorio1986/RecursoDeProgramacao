deba6ccfed46698770b3de55e5a6c18b8535d795
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-22 21:05:55 -0300

    Desenvolveno sempre com vonatde de aprender e voluir, smepre rfevisando a tecnologia

---
+++ Aula09/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Aula09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================Operadores Lógicos=================");
+
+            bool isLogged = true;
+            bool hasAdminAcess = false;
+
+            Console.WriteLine("Informações de Usuário:");
+            Console.WriteLine("Usuario Logado: " + isLogged);
+            Console.WriteLine("Acesso ao painel de administrador:" + hasAdminAcess);
+
+            Console.WriteLine("\nPermissões");
+
+            //Operador (E) &&
+
+            if (isLogged == true && hasAdminAcess == true)
+            {
+                Console.WriteLine("Acesso ao painel de administrador permitido");
+            }
+            else
+            {
+                Console.WriteLine("Acesso negado!");
+            }
+
+            //OPerador (||) - Ou
+            if(isLogged || hasAdminAcess)
+            {
+                Console.WriteLine("Acesso ao painel de administrador permitido");
+            }
+            else
+            {
+                Console.WriteLine("Acesso negado!");
+            }
+
+            //Operador NOT (!)
+            if(!isLogged)
+            {
+                Console.WriteLine("Usuário não está logado!");
+            }
+            else
+            {
+                Console.WriteLine("Usuário logado no sistema!");
+            }
+
+        }
+    }
+}
