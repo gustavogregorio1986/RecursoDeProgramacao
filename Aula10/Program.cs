830f9cf74c7e0a7e2d2e0aac647e224348f72753
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-24 08:36:45 -0300

    Desenvolvimento de sistemas com logica de programação

---
+++ Aula10/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Aula10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inicialização do programa
+            Console.WriteLine("==================Calculadora Simples============== \n");
+
+            Console.WriteLine("Digite o primeiro numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Selecionar o tipo de operação
+            Console.WriteLine("\nEscolha uma operação? ");
+            Console.WriteLine("1. Adição (+)");
+            Console.WriteLine("2. Subtração (-)");
+            Console.WriteLine("3. Multiplicação (*)");
+            Console.WriteLine("4. Divisão (/)");
+            Console.WriteLine("Digite o número da operação: ");
+            int operation = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0;
+
+            //Condições
+            if (operation == 1)
+            {
+                result = num1 + num2;
+            }
+            else if (operation == 2)
+            {
+                result = num1 - num2;
+            }
+            else if (operation == 3)
+            {
+                result = num1 * num2;
+            }
+            else if (operation == 4)
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida.");
+                return;
+            }
+
+            // Exibir resultado
+            Console.WriteLine("\nResultado: " + result);
+        }
+    }
+}
