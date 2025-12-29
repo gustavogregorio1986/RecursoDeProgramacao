6e05d72853935402bb45880b94fb5f6819008b1c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-29 14:29:13 -0300

    Desenvolvimentod e logica de programão

---
+++ Aiça18/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula17
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.File.WriteAllText("file.txt", "Hello World!");
+
+            Console.WriteLine("Arquivo criado com suceso!");
+        }
+    }
+}
